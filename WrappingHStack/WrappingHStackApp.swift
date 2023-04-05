4f869afa8d96ff1e2853c89e4a80bd213c18df0f
aleksykrolczyk <aleksykrolczyk@yahoo.pl>
2023-04-05 20:08:14 +0200

    Initial Commit

---
+++ WrappingHStack/WrappingHStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WrappingHStackApp.swift
+//  WrappingHStack
+//
+//  Created by Aleksy Krolczyk on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct WrappingHStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
